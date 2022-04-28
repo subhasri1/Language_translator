a2c91aab5a1d66e45f4fd310ae226bdebf0c2c3e
subrata101-tech <subratammondal692@gmmail.com>
2022-04-28 09:24:13 +0530

    created by subrata

---
+++ lt.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void registr(string, string); //subrata
+
+bool loginValidate(string,string);//subrata
+
+bool add(string); //manohor
+
+void display(); //dev
+
+bool remove(string);	// danish
+
+bool update(string,string,string); //harsh
+
+bool search(string);
+
+
